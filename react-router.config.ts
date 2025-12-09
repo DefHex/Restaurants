9fcf240cbc9bca691870a1269a0b07ac48dc3736
Roottoot <syuleiman@gmail.com>
2025-12-09 17:41:37 +0100

    change to no prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  prerender: true,
+  prerender: false,
   basename: viteConfig.base,
 } satisfies Config;
