9e9c1ce54c8141c90d4dbfbcb1482bdbf12f4f69
Roottoot <syuleiman@gmail.com>
2025-12-16 12:51:21 +0100

    changes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/Home.tsx"),
-route("restaurant", "routes/restaurant.tsx"),
-route("bar", "routes/bar.tsx"),
+export default [
+  index("routes/Home.tsx"),
+  route("restaurant", "routes/restaurant.tsx"),
+  route("bar", "routes/bar.tsx"),
+  route("buffet", "routes/buffet.tsx"),
 ] satisfies RouteConfig;
